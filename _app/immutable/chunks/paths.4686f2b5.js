b6ce031f8b58b7bce0b79d7b3e70a547b0733fa6
Taruna188 <tarunaprasetya@gmail.com>
2023-10-05 19:03:59 +0700

    Updates

--- _app/immutable/chunks/paths.4686f2b5.js
+++
@@ -1,1 +0,0 @@
-var s;const a=((s=globalThis.__sveltekit_1mbt1bx)==null?void 0:s.base)??"/fight-against-bullying";var t;const b=((t=globalThis.__sveltekit_1mbt1bx)==null?void 0:t.assets)??a;export{b as a,a as b};
